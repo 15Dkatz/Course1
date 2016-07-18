4764e7e5b679be0ecb44fb7f8187965cdd896a72
davidkatz <15Dkatz@shcp.edu>
2016-07-17 20:13:50 -0700

    cleaned up the ListView component

--- EventExpert/src/main.js
+++ EventExpert/src/main.js
@@ -4,7 +4,9 @@ import {
   StyleSheet,
   Text,
   View,
-  ListView
+  ListView,
+  TouchableOpacity,
+  Image
 } from 'react-native';
 
 const API_KEY='Bearer SZRBEN2CGEUPT57YVMXP';
@@ -15,18 +17,18 @@ module.exports = React.createClass({
   getInitialState() {
     return {
       dataSource: ds.cloneWithRows([
-        {
-          name: {
-            text: 'Event 1'
-          },
-          url: 'www.eventone.com'
-        },
-        {
-          name: {
-            text: 'Event 2'
-          },
-          url: 'www.eventtwo.com'
-        }
+        // {
+        //   name: {
+        //     text: 'Loading...'
+        //   },
+        // },
+        // {
+        //   name: {
+        //     text: 'Event 2'
+        //   },
+        //   url: 'www.eventtwo.com'
+        // }
+        ''
       ])
     }
   },
@@ -46,34 +48,59 @@ module.exports = React.createClass({
     })
     .then((response) => response.json())
     .then((responseJSON) => {
-      console.log('responseJSON', responseJSON);
+      // console.log('responseJSON', responseJSON);
       this.setState({
         dataSource: ds.cloneWithRows(responseJSON.events)
       })
     })
   },
 
+  details(rowData) {
+    console.log('open a detail page for', rowData);
+  },
+
   renderRow(rowData) {
+    console.log('rowData', rowData);
+    // let startTime = rowData.start.local;
+    if (rowData=='') {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    }
+
+    const img = rowData.logo.url;
+
     return (
       <View style={styles.row}>
-        <Text style={styles.rowText}>
-          {rowData.name.text}
-        </Text>
-        <Text>
-          {rowData.url}
-        </Text>
+        <Image
+          style={styles.rowLogo}
+          source={{uri: img}}
+        />
+        <View style={styles.rowDetails}>
+          <Text style={styles.title}>
+            {rowData.name.text}
+          </Text>
+          <TouchableOpacity
+            onPress={this.details(rowData)}
+          >
+            <Text style={styles.link}>
+              More Details
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   },
 
   render() {
     return (
-        <ListView
-          style={styles.list}
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => this.renderRow(rowData)}
-        />
-
+      <ListView
+        style={styles.list}
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => this.renderRow(rowData)}
+      />
     );
   }
 });
@@ -84,18 +111,36 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-
   list: {
     flex: 1,
-    borderColor: '#000',
-    borderWidth: 1
   },
-
   row: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    borderColor: '#ff0000',
+    borderColor: '#000',
+    borderWidth: 1,
+    padding: 15,
+    flexDirection: 'row'
+  },
+  title: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  link: {
+    color: 'blue'
+  },
+
+  rowLogo: {
+    flex: 1,
+    width: 50,
+    height: 50,
+    borderColor: '#000',
     borderWidth: 1
   },
+  rowDetails: {
+    flex: 4,
+    alignItems: 'center'
+  }
 });
